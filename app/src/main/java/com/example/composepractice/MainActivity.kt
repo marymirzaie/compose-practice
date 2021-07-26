f7aa91f942b47af2e4b073c9e86b8d8102eaacdd
Maryam Mirzaie <mary.mirzaie@divar.ir>
2021-07-26 13:42:26 +0430

    basics of compose

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -1,64 +1,101 @@
 package com.example.composepractice
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewsStory()
+            MyApp {
+                MyScreenContent()
+            }
         }
     }
 }
 
 @Composable
-fun NewsStory() {
-    MaterialTheme {
-        val typography = MaterialTheme.typography
-        Column(modifier = Modifier.padding(16.dp)) {
-            Image(painter = painterResource(id = R.drawable.sample_pic),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(180.dp)
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(4.dp)),
-                contentScale = ContentScale.Crop)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text("A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
-                style = typography.h6,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis)
-            Text("Davenport, California", style = typography.body2)
-            Text("December 2018", style = typography.body2)
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 5) Color.Green else Color.White
+        )
+    ) {
+        Text(text = "I've been clicked $count times")
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposePracticeTheme {
+        // A surface container using the 'background' color from the theme
+        content()
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(names) { name ->
+            Greeting(name = name)
+            Divider(color = Color.Black)
         }
     }
 }
 
-@Preview
-//best practice: create separate preview functions that aren't called by the app.
-// improves performance, makes it easier to have multiple previews later on
+@Preview("MyScreenPreview")
 @Composable
-fun PreviewGreeting() {
-    NewsStory()
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
+    val counterState = remember {
+        mutableStateOf(0)
+    }
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names = names, modifier = Modifier.weight(1f))
+        Counter(counterState.value) {
+            counterState.value = it
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+    Text(text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected }))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        Greeting(name = "Android")
+    }
 }
