c003397edd8b14993a21350b5b2e9d128259c48d
Maryam Mirzaie <mary.mirzaie@divar.ir>
2021-08-08 17:13:50 +0430

    Fix gradle stuff

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -10,14 +10,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,38 +31,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewsStory()
+            ComposePracticeTheme {
+                PhotoGraphCard()
+            }
         }
     }
 }
 
 @Composable
-fun NewsStory() {
-    MaterialTheme {
-        val typography = MaterialTheme.typography
-        Column(modifier = Modifier.padding(16.dp)) {
-            Image(painter = painterResource(id = R.drawable.sample_pic),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(180.dp)
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(4.dp)),
-                contentScale = ContentScale.Crop)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text("A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
-                style = typography.h6,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis)
-            Text("Davenport, California", style = typography.body2)
-            Text("December 2018", style = typography.body2)
+fun PhotoGraphCard() {
+    Column {
+        Text(text = "Alfred Sisly", fontWeight = FontWeight.Bold)
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
         }
     }
 }
 
 @Preview
-//best practice: create separate preview functions that aren't called by the app.
-// improves performance, makes it easier to have multiple previews later on
 @Composable
-fun PreviewGreeting() {
-    NewsStory()
+fun PhotoGraphCardPreview() {
+    ComposePracticeTheme {
+        PhotoGraphCard()
+    }
 }
