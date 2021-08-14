af1090d44fff9b91b194daaaa9ea3a6f0768a61a
Maryam Mirzaie <mary.mirzaie@divar.ir>
2021-08-14 14:56:08 +0430

    Working with lists slot api modifier

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -4,54 +4,158 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.composepractice.ui.theme.ComposePracticeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                PhotoGraphCard()
+                SimpleList()
             }
         }
     }
 }
 
 @Composable
-fun PhotoGraphCard() {
+fun SimpleList() {
+    val scrollState = rememberLazyListState()
+    val listSize = 100
+    val coroutineScope = rememberCoroutineScope()
     Column {
-        Text(text = "Alfred Sisly", fontWeight = FontWeight.Bold)
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to Bottom")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(index = it)
+            }
         }
     }
 }
 
-@Preview
 @Composable
-fun PhotoGraphCardPreview() {
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+@Preview
+fun SimpleListPreview() {
     ComposePracticeTheme {
-        PhotoGraphCard()
+        SimpleList()
+    }
+}
+
+@Composable
+fun LayoutsCodeLab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Layouts Codelab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(Modifier.padding(innerPadding)) {
+            BodyContent(modifier = Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
+        }
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi There!")
+        Text(text = "Hola")
+    }
+}
+
+@Composable
+fun PhotoGraphCard(modifier: Modifier = Modifier) {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable { /*do something*/ }
+        .padding(16.dp)) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            //image
+        }
+        Column(Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)) {
+            Text(text = "Alfred Sisly", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
     }
 }
